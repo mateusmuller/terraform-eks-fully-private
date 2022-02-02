c3e3373260399c5c7dddc0af3288c95edde3190c
Mateus Muller <mateusmuller@github.com>
2022-02-02 14:02:32 +0000

    refactor: Getting CIDR from variable

--- modules/network/vpc-endpoints.tf
+++ modules/network/vpc-endpoints.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "sg_vpc_endpoint" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [ "10.0.0.0/16" ]
+    cidr_blocks      = [ var.cidr_block ]
   }
 
   tags = var.tags
@@ -20,6 +20,11 @@ resource "aws_vpc_endpoint" "vpce_ecr_api" {
 
   security_group_ids = [ aws_security_group.sg_vpc_endpoint.id ]
 
+  subnet_ids = [
+    aws_subnet.subnet_1a.id,
+    aws_subnet.subnet_1b.id
+  ]
+
   private_dns_enabled = true
 }
 
@@ -29,6 +34,11 @@ resource "aws_vpc_endpoint" "vpce_ecr_dkr" {
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [ aws_security_group.sg_vpc_endpoint.id ]
+
+  subnet_ids = [
+    aws_subnet.subnet_1a.id,
+    aws_subnet.subnet_1b.id
+  ]
 
   private_dns_enabled = true
 }
@@ -40,6 +50,11 @@ resource "aws_vpc_endpoint" "vpce_ec2" {
 
   security_group_ids = [ aws_security_group.sg_vpc_endpoint.id ]
 
+  subnet_ids = [
+    aws_subnet.subnet_1a.id,
+    aws_subnet.subnet_1b.id
+  ]
+
   private_dns_enabled = true
 }
 
@@ -49,6 +64,11 @@ resource "aws_vpc_endpoint" "vpce_sts" {
   vpc_endpoint_type = "Interface"
 
   security_group_ids = [ aws_security_group.sg_vpc_endpoint.id ]
+
+  subnet_ids = [
+    aws_subnet.subnet_1a.id,
+    aws_subnet.subnet_1b.id
+  ]
 
   private_dns_enabled = true
 }
@@ -60,6 +80,11 @@ resource "aws_vpc_endpoint" "vpce_logs" {
 
   security_group_ids = [ aws_security_group.sg_vpc_endpoint.id ]
 
+  subnet_ids = [
+    aws_subnet.subnet_1a.id,
+    aws_subnet.subnet_1b.id
+  ]
+
   private_dns_enabled = true
 }
 
@@ -67,4 +92,8 @@ resource "aws_vpc_endpoint" "vpce_s3" {
   vpc_id            = aws_vpc.vpc.id
   service_name      = "com.amazonaws.${data.aws_region.region.name}.s3"
   vpc_endpoint_type = "Gateway"
+
+  route_table_ids = [
+    aws_route_table.route_table.id
+  ]
 }
